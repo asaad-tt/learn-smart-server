3c4b48ea23b0a48c1555577acb0c05b113c4fc79
asaad-tt <aasadshumon86@gmail.comm>
2023-01-15 22:08:17 +0600

    server make

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { config } from "dotenv";
+// import course from "./routes/courseRoutes.js";
+
+config({
+  path: "./config/config.env",
+});
+
+const app = express();
+
+import course from "./routes/courseRoutes.js";
+import user from "./routes/userRoutes.js";
+import ErrorMiddleware from "./middlewares/Error.js";
+
+app.use("/api/v1", course);
+app.use("/api/v1", user);
+
+export default app;
+
+app.use(ErrorMiddleware);
